43a0ffdb3b63c00bc68ce7f8183be984518ef2bb
garv <gsgojariya196003@gmail.com>
2024-03-11 16:16:03 +0530

    add commnet, like, playlist, tweet model

---
+++ src/models/playlist.model.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const playlistSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            index: true // indexes the field for faster search
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        videos: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Video'
+            }
+        ],
+        discription:{
+            type: String,
+            required: true
+        }
+    },
+    { timestamps: true }
+)
+
+export const Playlist = mongoose.model('Playlist', playlistSchema);
